b4f881e1bd3f39a8c6c569bc3e575b548e79ab83
XxSharkDesertxX <xx.alirahjo.xx@gmail.com>
2022-06-06 22:05:46 +0430

    06-Mini_project

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 new Vue({
     el:"#app",
+
     data:{
-        mes:"salam",
-        meshtml:"</a>Salam<a>",
-        mesif:true,
-        meselse:true,
-        numbers:[1,2,3,4,5,6,7],
-        url:"https://www.google.com",
-        title:"گوگل",
-        newTitle:new Date(),
+        plans:[
+            {name:"unlimeted" , price:500},
+            {name:"profetenal" , price:400},
+            {name:"heige" , price:300},
+            {name:"modeum" , price:200},
+            {name:"free" , price:0},
+        ],
+        activated:{},
+    },
+
+    methods: {
+        setActive:function(plan){
+            this.activated = plan;
+        }
+    },
+
+    components:{
+        plan:{
+            template:"#plan-template",
+            props:['plan','active'],
+            methods:{ 
+                activate:function () {
+                    this.$emit("activate", this.plan);
+                }
+            },
+            computed:{
+                isactive:function () {
+                    return (this.active == this.plan);
+                },
+                isUpgrade:function () {
+                    return (this.active.price < this.plan.price);
+                }
+            }
+        }
     }
+
 });
